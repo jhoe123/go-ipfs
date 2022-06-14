da9a63eae8abfd040d4218129a5f67471e1783c3
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-06-14 10:39:54 +0000

    updated elabox packaging params

--- elabox/install_scripts/postinstall.sh
+++ elabox/install_scripts/postinstall.sh
@@ -2,11 +2,11 @@
 
 IPFS_PATH=/home/elabox/data/ipfs
 export IPFS_PATH=$IPFS_PATH
-mkdir $IPFS_PATH
 
 if ! grep -q 'export IPFS_PATH' ~/.bashrc ; then
     printf "\nexport IPFS_PATH=$IPFS_PATH" >> ~/.bashrc
 fi
 
 # initialize
+echo "intializing ipfs"
 ./ipfs init -p server
