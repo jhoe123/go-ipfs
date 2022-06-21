1a4618c28b9c736ba6580639fdc82be70effd02c
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-06-21 11:21:05 +0000

    updated scripts

--- elabox/install_scripts/postinstall.sh
+++ elabox/install_scripts/postinstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 IPFS_PATH=/home/elabox/data/ipfs
+config_path=$IPFS_PATH/config
 export IPFS_PATH=$IPFS_PATH
 
 if ! grep -q 'export IPFS_PATH' ~/.bashrc ; then
@@ -10,3 +11,12 @@ fi
 # initialize
 echo "intializing ipfs"
 ./ipfs init -p server
+
+# update config
+tmp=$(mktemp)
+config=$(jq '.API.HTTPHeaders."Access-Control-Allow-Methods" = ["GET","PUT","POST"]' $config_path)
+config=$(echo $config | jq '.API.HTTPHeaders."Access-Control-Allow-Origin" = ["*"]')
+config=$(echo $config | jq '.Addresses.API="/ip4/0.0.0.0/tcp/5001"')
+echo $config | jq '.Addresses.Gateway="/ip4/0.0.0.0/tcp/5003"' > $tmp
+mv $tmp $config_path
+
